f8cab6df0b983f8f104544eb1a19d6470167605e
muthigani <muthigan@gmail.com>
2018-09-28 05:41:52 +0300

    Add files via upload

--- tests/api/v1/test_views.py
+++ tests/api/v1/test_views.py
@@ -14,14 +14,14 @@ class TestingEndpoints(unittest.TestCase):
     
     def test_to_get_all_orders(self):
         """These tests check  all orders """ 
-        response = self.app.get('/api/v1/orders')
+        response = self.app.get('/api/v1/orders', content_type="application/json")
         self.assertTrue(response.status_code, 200)
 
        
 
     def test_get_specific_order(self):
         """These tests check  specific order """ 
-        response = self.app.get('/api/v1/orders/1')
+        response = self.app.get('/api/v1/orders/1', content_type="application/json")
         self.assertTrue(response.status_code, 200)
 
     def test_error_id(self):
@@ -35,23 +35,23 @@ class TestingEndpoints(unittest.TestCase):
 
     def test_nonexistant_orderid(self):
         """Test check none existing orderid """
-        response = self.app.get('/api/v1/orders/20')
+        response = self.app.get('/api/v1/orders/20', content_type="application/json")
         self.assertEqual(response.status_code,404)
    
     
+    def test_error_when_string_is_notpassed(self):
+        """Test whether food  parameter is a string"""
+        response = self.app.post('/api/v1/orders',data = json.dumps({"food": 7}), 
+                                content_type="application/json", follow_redirects=True)
+        self.assertRaises(TypeError, response)
+    
 
-    def test_for_mandatory_parameter_missing_in_placed_order(self):
-        """Test to checks if mandatory parameter food is not passed"""
+    def test_mandatory_parameter_missing_in_placed_order(self):
+        """Test if mandatory parameter food is not passed"""
         response = self.app.post('/api/v1/orders',data = json.dumps({"Description": "Tasty food"}), 
                                 content_type="application/json", follow_redirects=True)
         self.assertEqual(response.status_code,404)
 
-    
-    def test_error_when_str_is_notpassed_as_parameter(self):
-        """Test to check whether food  parameter is a string"""
-        response = self.app.post('/api/v1/orders',data = json.dumps({"food": 7}), 
-                                content_type="application/json", follow_redirects=True)
-        self.assertRaises(TypeError, response)
 
 def tearDown(self):
         pass
